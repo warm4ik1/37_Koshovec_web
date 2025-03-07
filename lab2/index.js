0deeb93bd66da358ec815a7b603801b8e71675bd
warm4ik <kogbbccv@gmail.com>
2025-03-07 21:50:19 +0300

    refactoring structure + reset.css + grid

--- lab2/index.js
+++ lab2/index.js
@@ -1,1 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const purchaseBtn = document.getElementById('purchaseBtn');
+    const screamerOverlay = document.getElementById('overlay');
+    const screamSound = document.getElementById('sound');
 
+    function showScreamer() {
+        screamerOverlay.style.display = 'flex';
+        screamSound.volume = 1.0;
+        screamSound.currentTime = 0;
+        screamSound.play();
+    }
+
+    function hideScreamer() {
+        screamerOverlay.style.display = 'none';
+        screamSound.pause();
+    }
+
+    purchaseBtn.addEventListener('click', showScreamer);
+    screamerOverlay.addEventListener('click', hideScreamer);
+});
